07c1950ec9e42bfd20364ffb9e9cdd81342fe4cd
nandu507 <nsankar507@gmail.com>
2016-08-31 12:22:48 +0530

    arrange code using rubocop

--- sq-of-hash/sq_of_hash.rb
+++ sq-of-hash/sq_of_hash.rb
@@ -3,22 +3,22 @@ class Square
     count = 0
     count1 = 0
     x.times do
-      count+=1
+      count += 1
       x.times do
-          count1 += 1
-        if (count == 1)||(count ==x)
-          print "#"
-        elsif (count1 == 1)||(count1 == x)
-          print "#"
+        count1 += 1
+        if (count == 1) || (count == x)
+          print '#'
+        elsif (count1 == 1) || (count1 == x)
+          print '#'
         else
-          print " "
+          print ' '
         end
       end
-      puts"\n"
+      puts "\n"
       count1 = 0
     end
   end
 end
-print"Enter the one side length of sq :"
+print'Enter the one side length of sq :'
 side = gets.to_i
 Square.generate(side)
